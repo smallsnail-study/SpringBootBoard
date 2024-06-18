e9023d5368a9f5664b65661a1888d4878028cc8f
yunhuiGwak <kwakyh17@naver.com>
2024-06-18 17:15:34 +0900

    test: 생성자를 직접 작성한 test class HelloLombok.java - 롬복을 사용하지 않는다면 직접 생성자를 작성해야 한다. - 롬복으로 작성한 코드와 비교용

--- src/main/java/com/mysite/sbb/HelloLombok.java
+++ src/main/java/com/mysite/sbb/HelloLombok.java
@@ -1,19 +1,17 @@
 package com.mysite.sbb;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-// 속성에 final 추가, @RequiredArgsConstructor을 적용하면
-// 해당 속성(hello, lombok)을 필요로 하는 생성자가 롬복에 의해 자동생성
-@RequiredArgsConstructor // 파라미터가 없는 디폴트 생성자를 자동으로 생성
 @Getter
 public class HelloLombok {
 
-    /* final 은 자료형과 변수 등을 변경할 수 없게 만드는 키워드이다.
-만약 클래스 속성을 정의한 코드에 final이 없다면 생성자에 포함되지 않는다.
-또한 final을 적용하면 속성값을 변경할 수 없기 때문에 @Setter는 의미가 없어지고,  Setter 메서드 또한 사용할 수 없다*/
     private final String hello;
     private final int lombok;
+
+    // 롬복을 사용하지 않을 경우 생성자를 직접 작성해야 한다.
+    public HelloLombok(String hello, int lombok) {
+        this.hello = hello;
+        this.lombok = lombok;
+    }
 
     public static void main(String[] args) {
         HelloLombok helloLombok = new HelloLombok("헬로", 5);
