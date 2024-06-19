403a6b2d56f49ede2873daf10840197487f518ce
yunhuiGwak <kwakyh17@naver.com>
2024-06-19 17:29:59 +0900

    test: 컨트롤러 생성 후 URL 매핑 리턴값 수정 반영 테스트 MainController.java - URL 매핑 리턴값을 변경하고 수정한 내용이 반영되는지 테스트

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -9,6 +9,6 @@ public class MainController {
     @GetMapping("/sbb") // 요청된 URL(/sbb)과의 매핑 담당
     @ResponseBody // URL요청에 대한 응답으로 메서드의 타입을 리턴
     public String index() { // 응답을 받기 위해 void -> String으로 변경
-        return "index";
+        return "안녕하세요 sbb에 오신 것을 환영합니다.";
     }
 }
