c8635dba902dfc790bfd56b40031f84b808807f9
yunhuiGwak <kwakyh17@naver.com>
2024-06-19 17:28:10 +0900

    test: 컨트롤러 생성 후 URL 매핑 리턴값 테스트 MainController.java - @ResponseBody URL요청에 대한 응답으로 메서드의 타입을 리턴 - @ResponseBody를 생략한다면 스프링 부트는 ‘index’라는 문자열을 리턴하는 대신 index라는 이름의 템플릿 파일을 찾게 된다. - index 메서드 응답을 받기 위해 void -> String으로 변경

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,11 +2,13 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 스프링 부트의 컨트롤러가 된다.
 public class MainController {
     @GetMapping("/sbb") // 요청된 URL(/sbb)과의 매핑 담당
-    public void index() {
-        System.out.println("index"); // 콘솔 로그로 index 메서드 호출여부 확인 가능(결과값을 리턴하지 않아 500에러가 발생한다.)
+    @ResponseBody // URL요청에 대한 응답으로 메서드의 타입을 리턴
+    public String index() { // 응답을 받기 위해 void -> String으로 변경
+        return "index";
     }
 }
