427f0b00a4f3aea05f05726e1a0ddeb930bb32a4
yunhuiGwak <kwakyh17@naver.com>
2024-06-25 15:12:47 +0900

    feat: 질문 엔티티에서 답변 참조 가능하도록 추가 Question.java - 질문 하나에 답변은 여러 개이므로 Question 엔티티에 추가할 Answer 속성은 List 형태로 구성해야 한다. - Answer 객체들로 구성된 answerList를 Question 엔티티의 속성으로 추가하고 @OneToMany 애너테이션 설정     - 질문에서 답변을 참조하려면 `question.getAnswerList()` 를 호출하면 된다.     - `mappedBy` : 참조 엔티티의 속성명을 정의. (Answer 엔티티에서 Question 엔티티를 참조한 속성인 question을 mappedBy에 전달해야 한다.)     - `CascadeType.REMOVE` : 질문 삭제 시 그에 달린 답변들도 함께 삭제되도록 설정

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Getter
@@ -22,4 +23,7 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
 }
