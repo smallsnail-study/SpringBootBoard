dedb83fa83bc9136167ef8945d40000ddc0dd631
yunhuiGwak <kwakyh17@naver.com>
2024-06-26 15:02:03 +0900

    test: 질문 데이터 조회 test SbbApplicationTests.java question 테이블에 저장된 모든 데이터를 조회하기 위해서 리포지터리(questionRepository)의 findAll 메서드를 사용 - `findAll()` : H2 콘솔에서 입력해 본 `SELECT*FROM QUESTION` 과 같은 결과를 얻을 수 있다.
    앞서 2개의 질문 데이터를 저장했기 때문에 데이터 사이즈는 2가 되어야 한다.
    
    - `assertEquals(기대값, 실제값)` : 테스트에서 예상한 결과와 실제 결과가 동일한지를 확인하는 목적으로 사용. 기대값과 실제값이 동일하지 않다면 테스트는 실패로 처리된다.

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -14,17 +16,11 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2, all.size());
 
-        Question q2 = new Question();
-        q2.setSubject("스프링 부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
     }
 
 }
