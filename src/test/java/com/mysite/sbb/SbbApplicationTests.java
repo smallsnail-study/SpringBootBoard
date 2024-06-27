aaab18ab0eda9900369566a3df9dc24f9a704c25
yunhuiGwak <kwakyh17@naver.com>
2024-06-27 15:34:05 +0900

    test: 질문 데이터 삭제 test SbbApplicationTests.java - `this.questionRepository.count()` : 리포지터리의 count 메서드는 테이블 행의 개수를 리턴한다. - 리포지터리의 delete 메서드를 사용하여 데이터를 삭제했다. - 데이터 건수가 삭제하기 전에 2 였는데, 삭제한 후 1 이 되었는지를 테스트했다.

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,7 +3,10 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -14,11 +17,12 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
+        assertEquals(2, this.questionRepository.count());
         Optional<Question> oq = this.questionRepository.findById(1);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        q.setSubject("수정된 제목");
-        this.questionRepository.save(q);
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
     }
 
 }
