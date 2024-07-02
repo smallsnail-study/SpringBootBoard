22d8dd6dfafc41743e0cdb2837f07852848916ce
yunhuiGwak <kwakyh17@naver.com>
2024-07-02 18:15:34 +0900

    feat: QuestionController에 질문상세페이지URL 매핑 QuestionController.java - `@PathVariable` : 요청한 URL의 변하는 id값을 얻을 떄 사용 - `@GetMapping(value = “/question/detail/{id}”)`에서 사용한 `id`와 `@PathVariable(”id”)`의 매개변수 이름이 동일해야 한다.

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -18,5 +19,10 @@ public class QuestionController {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list"; // question_list.html을 리턴
+    }
+
+    @GetMapping("/question/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        return "question_detail";
     }
 }
